3660f552d61803950850798cf6a610ab7f5e3c8a
eto <alberto.v.ch@gmail.com>
2016-04-14 17:57:16 -0500

    14-04-2016_2

--- busca_reg.php
+++ busca_reg.php
@@ -9,10 +9,19 @@
 	echo '<fieldset><legend>BUSQUEDA DOCUMENTOS RECIBIDOS</legend>';
 	echo 'REGISTRO : <input type="text" name="n_reg" size="10"/> &nbsp 
 	 FECHA : <input type="text" name="fecha_recep" size="10"/></br>
-	 REMITENTE : <input type="text" name="nombre_rem" size="50"/></br>';
+	 REMITENTE : </br><input type="text" name="nombre_rem" size="50"/></br>';
 	echo '<input type="submit" name="enviar" value="BUSCA RECIBIDO"/>';
 	echo '</fieldset>';
 	echo '</form>';
+	
+	echo '
+	<script>
+		window.onload = function mifun() {
+			window.open("reporte.php", "up_left", 
+			"toolbar=yes, scrollbars=yes, resizable=yes, top=500, left=500, width=400, height=400")
+		}
+	</script>';
+	
 	echo '</td></tr></table></body></html>';
 	
 	
